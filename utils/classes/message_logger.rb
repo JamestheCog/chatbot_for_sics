b57d5163cbf8ed94cb6e3c70e6e3c167b108a54b
JamestheCog <jcog196013@gmail.com>
2025-10-31 12:05:55 +0800

    Removed puts calls
    I put 'puts' there to debug a couple of things initially

--- utils/classes/message_logger.rb
+++ utils/classes/message_logger.rb
@@ -21,16 +21,17 @@ class MessageLogger
         raise ArgumentError, "'message' and 'role' need to be strings" unless message.is_a?(String) && role.is_a?(String)
         raise ArgumentError, "'role' should either be of value 'user' or 'model'" unless ['user', 'model'].include?(role)
         raise ArgumentError, "'base64_img' needs to be a nil or a string." unless base64_img.nil? || base64_img.is_a?(String)
-        if !base64_img.nil? && base64_img.is_a?(String) && base64_img.trim.length > 0
-            if img_mimetype.nil? && !img_mimetype.is_a?(String) && img_mimetype.trim.length == 0
+        if !base64_img.nil? && base64_img.is_a?(String) && base64_img.strip.length > 0
+            if img_mimetype.nil? && !img_mimetype.is_a?(String) && img_mimetype.strip.length == 0
                 raise ArgumentError, "img_mimetype must be a string if base64_img is non-nil and a non-empty string."
             end
             img_mimetype.downcase!
             raise ArgumentError, "img_mimetype is invalid." unless img_mimetype.match?(/image\/(png|jpg)/)
         end
+
         store_message_in_cloud(@user_id, @conversation_id, role, message)
-        if base64_img.nil? || base64_img.strip.length == 0
-            @messages << {'role' => role, 'parts' => [{'text' => message}, {'inline_data' => {'mime_type' : img_mimetype, 'data' : base64_img}}]}
+        if !base64_img.nil? && !img_mimetype.nil?
+            @messages << {'role' => role, 'parts' => [{'text' => message}, {'inline_data' => {'mime_type' => img_mimetype, 'data' => base64_img}}]}
         else 
             @messages << {'role' => role, 'parts' => [{'text' => message}]}
         end
